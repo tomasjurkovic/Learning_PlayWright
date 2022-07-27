bf403747da8662d9637aad7eb7abc717a3e5d758
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-07-27 13:14:10 +0200

    hover and javascript popup window examples added
    added test example for hovering and testing of elements visible only when hovering
    added test example for javascript browser window popup

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -3,6 +3,7 @@ const {test, expect} = require("@playwright/test");
     // consts:
     const baseUrl = "https://www.rahulshettyacademy.com/AutomationPractice/";
     const secondUrl = "https://www.google.com";
+    const topEndpoint = "#top";
 
 test('Page navigation test', async ({page}) => {
     
@@ -32,7 +33,7 @@ test('Page navigation test', async ({page}) => {
     }
 );
 
-test.only('Popup validation test', async ({page}) => {
+test('Visible/hidden validation test', async ({page}) => {
 
     // navigate to main page:
     await page.goto(baseUrl);
@@ -51,5 +52,50 @@ test.only('Popup validation test', async ({page}) => {
 
     // check if displayed-text is visible
     await expect(page.locator("#displayed-text")).toBeVisible();
+    }
+);
+
+test('Popup validation test', async ({page}) => {
+
+    // navigate to main page:
+    await page.goto(baseUrl);
+
+    // click on allert button:
+    await page.locator("#alertbtn").click();
+
+    // when dialog window appears click on 'OK' button:
+    page.on('dialog', dialog => dialog.accept());
+    await page.locator("#confirmbtn").click();
+
+    // click on allert button:
+    await page.locator("#alertbtn").click();
+
+    // when dialog window appears click on 'Cancel' button:
+    page.on('dialog', dialog => dialog.dismiss());
+    }
+);
+
+test.only('Hover validation test', async ({page}) => {
+
+    // navigate to main page:
+    await page.goto(baseUrl);
+
+    // check if there are two hidden elements (Top and Reload):
+    await expect(page.locator("text=Top")).toBeHidden();
+    await expect(page.locator("text=Reload")).toBeHidden();
+
+    // hover over element
+    await page.locator("#mousehover").hover();
+
+    // check if there are two elements in dropdown (Top and Reload):
+    await expect(page.locator("text=Top")).toBeVisible();
+    await expect(page.locator("text=Reload")).toBeVisible();
+
+    // click on 'top' option from dropdown shown after hover
+    await page.locator('text=Top').click();
+
+    // check if URL changed
+    expect(page).toHaveURL(baseUrl + topEndpoint);
+
     }
 );
