7cfc9f5eb570dfacbafa16f672c07f79ab937e3e
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-07-28 14:28:50 +0200

    Update WebAPIPart1.spec.js
    login was set as variable in the beginning so it is accessible right now in other tests
    test is using only inserting token to local storage

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -2,6 +2,7 @@ const {test, expect, request} = require("@playwright/test");
 
 const loginUrl = "https://www.rahulshettyacademy.com/api/ecom/auth/login";
 const userPayload = {userEmail:"tomastest@test.org",userPassword:"Test1234"};
+let token;
 
 // Login automatically via API
 test.beforeAll( async() => {
@@ -20,46 +21,32 @@ test.beforeAll( async() => {
 
     // grab token from response
     const loginResponseJson = await loginResponse.json();
-    const token = await loginResponseJson.token;
+    token = await loginResponseJson.token;
     const statusCode = await loginResponse.status();
-
-    console.log(token);
 
     // expect if status is 200
     expect(statusCode).toEqual(200);
     }
 );
 
-test.only('RahulShetty eshop login with correct credentials test', async ({page}) => {
+test.only('Client app login test', async ({page}) => {
 
-    const mail = "tomastest@test.org";
-    const password = "Test1234";
-    const userEmailLocator = page.locator("#userEmail");
-    const passwordLocator = page.locator("#userPassword");
-    const loginLocator = page.locator("#login");
+    // here can be inserted javascript:
+    page.addInitScript(value => {
+        window.localStorage.setItem('token', value);
+    }, token );
+
     const mainPage = "https://www.rahulshettyacademy.com/client/";
-    const loginEndpoint = "auth/login";
     const dashEndpoint = "dashboard/dash";
     const title = "Let's Shop";
     
     // navigate to specific page
     await page.goto(mainPage);
 
-    await userEmailLocator.type(mail);
-    await passwordLocator.type(password);
-
-    await expect(page).toHaveURL(mainPage + loginEndpoint);
-
-    // click on login button
-    await loginLocator.click()
-
-    // load until data are loaded (card titles from API):
-    // "networkidle" - when all API calls are successfully completed:
-    await page.waitForLoadState('networkidle');
-    
-    await expect(page).toHaveTitle(title);
-
+    // verify if main page with dashboard appears (no login page because of added token):
     await expect(page).toHaveURL(mainPage + dashEndpoint);
 
+    // check it's title:
+    await expect(page).toHaveTitle(title);
     }
 );
