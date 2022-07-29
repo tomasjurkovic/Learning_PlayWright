39436c7fd32c8a730b77b8d2e86cdd6680fbb0d0
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-07-29 10:51:27 +0200

    Update WebAPIPart1.spec.js
    prepared test for order id created by api (needs some work still)

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -3,6 +3,13 @@ const {test, expect, request} = require("@playwright/test");
 const loginUrl = "https://www.rahulshettyacademy.com/api/ecom/auth/login";
 const userPayload = {userEmail:"tomastest@test.org",userPassword:"Test1234"};
 let token;
+const mainPage = "https://www.rahulshettyacademy.com/client/";
+const dashEndpoint = "dashboard/dash";
+const title = "Let's Shop";
+const orderUrl = "https://www.rahulshettyacademy.com/api/ecom/order/create-order";
+const contentType = "application/json";
+const orderPayload = {orders:[{country:"Cuba",productOrderedId:"6262e9d9e26b7e1a10e89c04"}]};
+const historyEndpoint = "dashboard/myorders";
 
 // Login automatically via API
 test.beforeAll( async() => {
@@ -29,17 +36,13 @@ test.beforeAll( async() => {
     }
 );
 
-test.only('Client app login test', async ({page}) => {
+test('Client app login test', async ({page}) => {
 
     // here can be inserted javascript:
     page.addInitScript(value => {
         window.localStorage.setItem('token', value);
     }, token );
 
-    const mainPage = "https://www.rahulshettyacademy.com/client/";
-    const dashEndpoint = "dashboard/dash";
-    const title = "Let's Shop";
-    
     // navigate to specific page
     await page.goto(mainPage);
 
@@ -49,4 +52,79 @@ test.only('Client app login test', async ({page}) => {
     // check it's title:
     await expect(page).toHaveTitle(title);
     }
+);
+
+test.only('Client app login test', async ({page}) => {
+
+    // verify if order is created in history page:
+    // precondition - created order (through API) -> check order history page
+    // here can be inserted javascript:
+    page.addInitScript(value => {
+        window.localStorage.setItem('token', value);
+    }, token );
+
+    // create order via API:
+    // authorization is made in header for this API
+    const apiContext = await request.newContext();
+
+    // use post method with token autorization 
+    const orderResponse = await apiContext.post(orderUrl, {
+        data: orderPayload,
+        headers: 
+            {
+                'Authorization': token,
+                'Content-Type': contentType,
+            }
+        }
+    );
+
+    // extract order id from response:
+    const orderResponseJson = await orderResponse.json();
+    orderID = await orderResponseJson.orders[0];
+
+    console.log(orderID);
+    
+    // expect if status is 201
+    const statusCode = await orderResponse.status();
+    expect(statusCode).toEqual(201);
+
+    // verify if order is located in history page:
+   // check url
+
+   page.goto(mainPage + historyEndpoint);
+
+   // usually it fails while table is fully loaded and wait for elements did not do its job
+   const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+       await delay(1000);
+   
+    const tbRows = await page.locator("tbody tr");
+       
+    // click on correct order (actual one)
+       for (let i = 1; i <= await tbRows.count(); i++) {
+        if (await page.locator("tbody tr:nth-child("+i+") th").textContent() === orderIdSliced) 
+        {
+           await page.locator("tbody tr:nth-child("+i+") td .btn-primary").click();
+               break;
+       }
+    }
+    // check if correct URL is displayed (for correct orderId)
+    const orderDetailEndpoint = "dashboard/order-details/" + orderID;
+    await expect(page).toHaveURL(mainPage + orderDetailEndpoint);
+   
+    // check if details are correct:
+    const orderIdDetails = await page.locator(".-main").textContent(); 
+    const emailDetails = await page.locator("div:nth-child(1) > div:nth-child(1) > p:nth-child(2)").textContent(); 
+    const productDetails = await page.locator(".title").textContent(); 
+    const countryDetails = await page.locator("div:nth-child(1) > div:nth-child(1) > p:nth-child(3)").textContent(); 
+    const priceDetails = await page.locator(".price").textContent(); 
+   
+    // let's check all values:
+    expect(await orderIdDetails).toEqual(orderID);
+    expect(await emailDetails).toEqual(' tomastest@test.org ');
+    expect(await countryDetails).toEqual(" Country - Cuba ");
+    expect(await productDetails).toEqual(' iphone 13 pro ');
+    expect(await priceDetails).toEqual(" $ 231500 ");
+
+    }
+
 );
